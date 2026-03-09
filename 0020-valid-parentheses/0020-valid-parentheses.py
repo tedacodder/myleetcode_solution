fb58da6cab6554199d0f709d77c83432b79720fd
tedacodder <155418230+tedacodder@users.noreply.github.com>
2026-03-09 18:37:32 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -6,6 +6,8 @@ class Solution:
             if stack and s[i] in dic and stack[-1]==dic[s[i]]:
                 stack.pop()
                 continue
+            elif stack and s[i] in dic and stack[-1]!=dic[s[i]]:
+                return False
             stack.append(s[i])
         return len(stack)==0
 
