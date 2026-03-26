8567541d20d7b817211bf703301c2de2096a3c14
tedacodder <155418230+tedacodder@users.noreply.github.com>
2026-03-26 19:40:37 +0300

    Time: 0 ms (100%), Space: 19.5 MB (52.23%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,10 +3,13 @@ class Solution:
         ans=[]
         def backtrack(start=0,path=[]):
             ans.append(path[:])
+            if len(path)==len(nums):
+                return
             for i in range(start,len(nums)):
                 path.append(nums[i])
                 backtrack(i+1,path)
                 path.pop()
         backtrack()
         return ans
+        
 
