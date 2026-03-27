d6016dadecc33f6fea96e49c7d4bf35a44548965
tedacodder <155418230+tedacodder@users.noreply.github.com>
2026-03-27 19:50:59 +0300

    Time: 8 ms (81.06%), Space: 19.7 MB (81.51%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        ans=[]
+        col=[False]*n
+        posDiag=[False]*(2*n-1)
+        negDiag=[False]*(2*n-1)
+        board=[["."]*n for _ in range(n)]
+        def backtrack(i=0):
+
+            if i==n:
+                ans.append(["".join(b) for b in board])
+                return
+            
+
+            for j in range(n):
+                posD=i+j
+                negD=i-j
+                if not col[j] and not posDiag[posD] and not negDiag[negD]:#only wanted cells
+                    col[j]=posDiag[posD]=negDiag[negD]=True
+                    board[i][j]="Q"
+                    backtrack(i+1)
+                    col[j]=posDiag[posD]=negDiag[negD]=False
+                    board[i][j]="."
+
+                
+        backtrack()
+        return ans
